337abbcbbe885e46e690f66196421af2f485c4c5
Michael Sobrepera <mjsobrep@live.com>
2022-01-01 23:27:03 -0800

    bump version for doc updates

--- setup.py
+++ setup.py
@@ -5,24 +5,26 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
+
 setup(name='quaternions',
-      version='0.5',
+      version='0.6',
       description='A package to handle quaternions',
       long_description=readme(),
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
       ],
       keywords='quaternion transformations',
       url='https://github.com/mjsobrep/quaternions',
       author='Michael Sobrepera',
       author_email='mjsobrep@live.com',
       license='MIT',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      packages=find_packages(
+          exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       install_requires=[],
       include_package_data=True,
       zip_safe=False,
